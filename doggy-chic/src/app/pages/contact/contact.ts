fce55aedfddad55341b9a57633476970fa6d1d62
LeaCavalli <lea.cavalli01@gmail.com>
2025-11-10 13:27:07 +0100

    home page desktop ok

--- doggy-chic/src/app/pages/contact/contact.ts
+++ doggy-chic/src/app/pages/contact/contact.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.scss',
+  styleUrls: ['./contact.scss']
 })
 export class Contact {
+  contactForm: FormGroup;
+  submitted = false;
+  success = false;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      mail: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.contactForm.valid) {
+      this.success = true;
+      console.log('Formulaire envoyé avec succès ✅', this.contactForm.value);
+
+      // Optionnel : reset du formulaire après une petite pause
+      setTimeout(() => {
+        this.contactForm.reset();
+        this.submitted = false;
+        this.success = false;
+      }, 4000);
+    }
+  }
+
+  // Accès simplifié aux champs
+  get f() {
+    return this.contactForm.controls;
+  }
 }
